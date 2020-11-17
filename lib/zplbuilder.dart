979a71ed6e6b606a13990b6208cb33b65c6980e2
asafeca <safeetyy@gmail.com>
2020-11-17 13:47:48 +0100

    expose just one class

--- lib/zplbuilder.dart
+++ lib/zplbuilder.dart
@@ -1,8 +1,3 @@
 library zplbuilder;
 
 export 'src/builder/builder.dart';
-export 'src/builder/ibuilder.dart';
-export 'src/components/flex.dart';
-export 'src/components/font.dart';
-export 'src/components/title.dart';
-export 'src/components/zpl_composer.dart';
